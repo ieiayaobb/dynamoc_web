e66c1cbc56f2f12f627c8d46dbcc22cb7ac64f03
Yineng Shen <ieiayaobb@qq.com>
2019-08-24 22:15:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var express = require('express');
-
 var server = express();
 
 var options = {
-
-index: 'dist/index.html'
-
+    index: 'dist/index.html'
 };
 
 server.use('/', express.static('/home/site/wwwroot', options));
